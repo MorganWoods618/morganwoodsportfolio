4804889ac5d1521e701e4cadfd4547210a540104
Morgan Woods <morgan4242564@gmail.com>
2023-01-30 21:22:18 -0700

    created moving circles

---
+++ src/pages/homePage.js
@@ -0,0 +1,20 @@
+import { Container, Row, Button } from "react-bootstrap";
+import "./homepage.css"
+
+const HomePage = () => {
+  return (
+    <div className="main">
+      <Container>
+        <Row>
+          <div class="Text">Homepage</div>
+        <div class="parent">
+        <div class="child"></div>
+        <div class="child2"></div>
+    </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default HomePage;
